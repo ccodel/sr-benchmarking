e2164b8f002f48741a6f7936ab3d5841ad4fdc69
Cayden Codel <ccodel@andrew.cmu.edu>
2024-05-16 02:22:46 -0400

    Updated README

--- run/run_lsr_check.sh
+++ run/run_lsr_check.sh
@@ -6,7 +6,7 @@ echo "Running lsr-check on all .lpr and .lsr proofs"
 echo "NOTE: Checking all proofs takes about 4 minutes in total"
 
 script_dir=$(dirname $0)
-#$script_dir/run_checker_base.sh lsr-check dsr-trim pr
+$script_dir/run_checker_base.sh lsr-check dsr-trim pr
 $script_dir/run_checker_base.sh lsr-check dsr-trim sr
 
 echo "Finished running lsr-check on all .lpr and .lsr proofs"
