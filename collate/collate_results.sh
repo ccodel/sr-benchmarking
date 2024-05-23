82511e60e81affada7353b50cde23c46a77d5d39
Cayden Codel <ccodel@andrew.cmu.edu>
2024-05-22 22:17:21 -0700

    Update collation scripts

--- collate/collate_results.sh
+++ collate/collate_results.sh
@@ -5,13 +5,22 @@
 #   results/dpr-trim
 #   results/cake_lpr
 #   results/dsr-trim
-#   results/ppr2drat
+#   results/sr2drat
+#   results/lrat-check
+#   results/lpr-check
+#   results/lsr-check
+#   results/lean-lsr-check
 # Stores collated results in results/collated
+
+starting_dir=$(pwd)
+script_dir=$(dirname $0)
+
+cd $script_dir/..
 
 mkdir -p results
 mkdir -p results/collated
 
-for checker in drat-trim dpr-trim cake_lpr dsr-trim ppr2drat lrat-check lpr-check lsr-check lean-lsr-check ; do
+for checker in cake_lpr drat-trim dpr-trim dsr-trim sr2drat lrat-check lpr-check lsr-check lean-lsr-check ; do
   if [ -d ./results/${checker} ]; then
     
     csv=results/collated/${checker}-collated.csv
@@ -25,3 +34,5 @@ for checker in drat-trim dpr-trim cake_lpr dsr-trim ppr2drat lrat-check lpr-chec
     echo "No results found for ${checker}, skipping..."
   fi
 done
+
+cd $starting_dir
---
+++ collate_all.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Collates all results
+# Just a wrapper for collate/collate_results.sh
+
+script_dir=$(dirname $0)
+${script_dir}/collate/collate_results.sh
