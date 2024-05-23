187ce283348ff3077a659b7654035f53c00f4d1f
Cayden Codel <ccodel@andrew.cmu.edu>
2024-05-22 22:23:33 -0700

    fix: run directory in check_all

--- check_all.sh
+++ check_all.sh
@@ -7,12 +7,12 @@
 starting_dir=$(pwd)
 script_dir=$(dirname $0)
 
-${script_dir}/run_lpr_check.sh
-${script_dir}/run_lsr_check.sh
-${script_dir}/run_cake_lpr.sh
+${script_dir}/run/run_lpr_check.sh
+${script_dir}/run/run_lsr_check.sh
+${script_dir}/run/run_cake_lpr.sh
 
 if [ -f ${script_dir}/${lean_dir}/${lean_checker} ]; then
-  ${script_dir}/run_lean_lsr_check.sh
+  ${script_dir}/run/run_lean_lsr_check.sh
 else
   echo "Lean checker doesn't exist, skipping..."
 end
