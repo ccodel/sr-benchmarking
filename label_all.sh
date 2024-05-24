7f210de70278a2dd08ea7c5766a709f42db1f72b
Cayden Codel <ccodel@andrew.cmu.edu>
2024-05-23 22:14:51 -0700

    fix: uncommented lines in label_all

--- label_all.sh
+++ label_all.sh
@@ -5,10 +5,10 @@
 script_dir=$(dirname $0)
 
 echo "Running dpr-trim on .pr proofs"
-#$script_dir/run/run_dpr_trim.sh
+$script_dir/run/run_dpr_trim.sh
 
 echo "Running dsr-trim on .sr proofs"
-#$script_dir/run/run_dsr_trim.sh
+$script_dir/run/run_dsr_trim.sh
 
 echo "Running sr2drat on .sr proofs"
 $script_dir/run/run_sr2drat.sh
